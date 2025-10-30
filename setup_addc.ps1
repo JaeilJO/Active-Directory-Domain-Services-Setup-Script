1a052cae137b1fcc6b98fb88712158d46cdccec8
JaeilJo <88572098+JaeilJO@users.noreply.github.com>
2025-10-31 08:12:25 +0900

    refactor: change code

--- setup_addc.ps1
+++ setup_addc.ps1
@@ -1,18 +1,18 @@
 Write-Host "`n[1/3] Network Configuration 시작`n"
 
-$ifAlias = (Get-NetAdapter | Where-Object Status -eq 'Up' | Select-Object -First 1 -ExpandProperty Name)
-$currentIpInfo = Get-NetIPAddress -InterfaceAlias $ifAlias -AddressFamily IPv4 | Select-Object -First 1
+$ifAlias = Get-Netadapter | Select-Object -ExpandProperty Name
+$currentIpInfo = Get-NetIPAddress -InterfaceAlias $ifAlias -AddressFamily IPv4
 
-$ipAddress = $currentIpInfo.IPAddress
-$prefix = $currentIpInfo.PrefixLength
-$defaultGateway = (Get-NetIPConfiguration -InterfaceAlias $ifAlias).IPv4DefaultGateway.NextHop
+$ipAddress = $currentIpInfo | Select-Object -ExpandProperty IPAddress
+$prefix = $currentIpInfo | Select-Object -ExpandProperty PrefixLength
+$defaultGateway = Get-netIPConfiguration -InterfaceAlias $ifAlias).IPv4DefaultGateway.NextHop
 
 Write-Host "Interface        : $ifAlias"
 Write-Host "Current IP       : $ipAddress"
 Write-Host "Prefix (CIDR)    : /$prefix"
 Write-Host "Default Gateway  : $defaultGateway"
 
-Write-Host "`nDHCP 비활성화 및 고정 IP 전환 중... (RDP 연결 시 주의)"
+Write-Host "`nDHCP 비활성화 및 고정 IP 전환 중... (RDP 연결 시 주의 중간에 끊길 수 있습니다.)"
 Set-NetIPInterface -InterfaceAlias $ifAlias -Dhcp Disabled -ErrorAction SilentlyContinue
 New-NetIPAddress -InterfaceAlias $ifAlias -IPAddress $ipAddress -PrefixLength $prefix -DefaultGateway $defaultGateway -ErrorAction SilentlyContinue
 Set-DnsClientServerAddress -InterfaceAlias $ifAlias -ServerAddresses $ipAddress
@@ -25,7 +25,7 @@ Import-Module ADDSDeployment -ErrorAction SilentlyContinue
 Write-Host "AD DS module loaded successfully.`n"
 
 Write-Host "[3/3] Configuring AD Forest`n"
-$dsrm = Read-Host "Enter DSRM password" -AsSecureString
+$dsrm = Read-Host "DSRM password를 입력해주세요" -AsSecureString
 Install-ADDSForest -DomainName "jestad.com" -DomainNetbiosName "JESTAD" -SafeModeAdministratorPassword $dsrm -InstallDns -Force
 
 Write-Host "`nAD DS installation and forest configuration complete."
